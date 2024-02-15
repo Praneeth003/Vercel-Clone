6b31f53001481c3c2281d8855de2f6cf87e417e0
praneeth003 <ch.krishnapraneeth@gmail.com>
2024-02-14 21:09:18 -0500

    define get route for deploy

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,12 @@ import cors from "cors";
 
 const app = express();
 app.use(cors());
+app.use(express.json());
+
+app.get('/deploy', (req,res) => {
+    const repoUrl = req.body.repoUrl;
+    console.log(repoUrl);
+})
 
 app.listen(4000, () => {
     console.log('Server is running on port 4000');
