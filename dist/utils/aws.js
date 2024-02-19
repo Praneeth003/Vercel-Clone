837888c8a55b8f9d4a1d2af273ef201d04067ebe
praneeth003 <ch.krishnapraneeth@gmail.com>
2024-02-19 13:47:14 -0500

    create upload files to bucket function

--- dist/utils/aws.js
+++ dist/utils/aws.js
@@ -1,53 +1,35 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const aws_sdk_1 = __importStar(require("aws-sdk"));
+exports.uploadFile = void 0;
+const aws_sdk_1 = require("aws-sdk");
+const fs_1 = __importDefault(require("fs"));
 const dotenv_1 = __importDefault(require("dotenv"));
-// Load environment variables from .env file
 dotenv_1.default.config();
-// Configure AWS credentials
-aws_sdk_1.default.config.update({
+// Create an S3 instance with my credentials
+const s3 = new aws_sdk_1.S3({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: process.env.AWS_REGION
 });
-// Create an S3 instance
-const s3 = new aws_sdk_1.S3();
-// Specify the bucket name
-const params = {
-    Bucket: 'vercelclone'
-};
-// Try to list the objects in the bucket
-s3.listObjects(params, (err, data) => {
-    if (err) {
-        console.log('Error', err);
-    }
-    else {
-        console.log('Success', data);
-    }
+// Upload a file to my bucket
+const uploadFile = (fileName, localFilePath) => __awaiter(void 0, void 0, void 0, function* () {
+    const fileContent = fs_1.default.readFileSync(localFilePath);
+    const response = yield s3.upload({
+        Body: fileContent,
+        Bucket: 'vercelclone',
+        Key: fileName,
+    }).promise();
+    console.log(response);
 });
+exports.uploadFile = uploadFile;
--- src/utils/aws.ts
+++ src/utils/aws.ts
@@ -1,29 +1,23 @@
-import AWS, { S3 } from 'aws-sdk';
+import { S3 } from 'aws-sdk';
+import fs from 'fs';
 import dotenv from 'dotenv';
 
-// Load environment variables from .env file
+
 dotenv.config();
 
-// Configure AWS credentials
-AWS.config.update({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION
+// Create an S3 instance with my credentials
+const s3 = new S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
 });
 
-// Create an S3 instance
-const s3 = new S3();
-
-// Specify the bucket name
-const params = {
-  Bucket: 'vercelclone'
-};
-
-// Try to list the objects in the bucket
-s3.listObjects(params, (err, data) => {
-  if (err) {
-    console.log('Error', err);
-  } else {
-    console.log('Success', data);
-  }
-});
+// Upload a file to my bucket
+export const uploadFile = async (fileName: string, localFilePath: string) => {
+  const fileContent = fs.readFileSync(localFilePath);
+  const response = await s3.upload({
+    Body: fileContent,
+    Bucket: 'vercelclone',
+    Key: fileName,
+  }).promise();
+  console.log(response);
+}
